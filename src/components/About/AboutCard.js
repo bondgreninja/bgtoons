0b906a36acde60d4c7d865f32ad45af10be69dff
Sunil Gupta <131786207+skgupta507@users.noreply.github.com>
2024-02-29 16:57:30 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,8 +8,8 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{textAlign: 'justify'}}>
-            Hi Everyone, I am <span className="purple">Mohit Sojitra </span>
-            from <span className="purple"> Gujarat, India.</span>
+            Hi Everyone, I am <span className="purple">Sunil Gupta </span>
+            from <span className="purple"> Karnataka, India.</span>
             <br />I am a Full Stack Javascript Developer.
             <br />
             <br />
